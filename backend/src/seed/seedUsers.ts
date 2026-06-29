68b22e1025d51865e6f8ac8d70438a83cd60e648
Cecilia <cecilia.lisci.01@gmail.com>
2026-06-29 23:32:52 +0200

    Ulteriori modifiche

--- backend/src/seed/seedUsers.ts
+++ backend/src/seed/seedUsers.ts
@@ -1,13 +1,22 @@
-import "dotenv/config";
-import sequelize from "../config/database";
-import bcrypt from "bcrypt";
-import User from "../auth/User";
+import "dotenv/config"; // caricamento della variabile d'ambiente dal file .env
+import sequelize from "../config/database"; // import dell'istanza di connessione al database
+import bcrypt from "bcrypt"; // import della libreria per cifrare le password
+import User from "../auth/User"; // import del modello utenti
+
+/**
+ * la funzione seed inizializza il database creando, se assente, un utente amministrativo di default.
+ */
 
 async function seed() {
+
+  // la funzione sincronizza i modelli con il daatabase
   await sequelize.sync();
 
+  // in hashedPassword salva l'hash della password "admin123" una volta generato
   const hashedPassword = await bcrypt.hash("admin123", 10);
 
+  // cerca attraverso il metodo sequelize un utente con username admin, e se non esiste lo crea secondo
+  // i valori riportati di seguito
   await User.findOrCreate({
     where: { username: "admin" },
     defaults: {
@@ -15,9 +24,10 @@ async function seed() {
       role: "ADMIN"
     }
   });
-
+  // la funzione quindi stampa un messaggio di corretta esecuzione delle operazioni
   console.log("Utente admin creato correttamente");
+  // tale comando termina l'esecuzione dello script e restituisce al sistema operativo il codice di uscita 0 (=operazione conclusa con successo) 
   process.exit(0);
 }
-
+// questo comando avvia l'inizializzazione del database
 seed();
